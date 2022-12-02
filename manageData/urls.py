863659b465acd3638b6ce936689b5ef10a8a0a38
mahdi imani wafi <73066863+mahdiwafi@users.noreply.github.com>
2022-12-02 19:54:19 +0700

    manageData working Backend

--- manageData/urls.py
+++ manageData/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import home_admin
+from .views import *
 
 app_name = 'manageData'
 urlpatterns = [
     path('admin_home', home_admin, name='home_admin'),
+    path('admin_home/view-destination-area', view_destination_area, name='view-destination-area'),
+    path('admin_home/add-destination-area', add_destination_area, name='add-destination-area'),
+    path('admin_home/view-site', view_site, name='view-site'),
+    path('admin_home/add-site', add_site, name='add-site'),
+    path('admin_home/view-accommodation', view_accommodation, name='view-accommodation'),
+    path('admin_home/add-accommodation', add_accommodation, name='add-accommodation'),
 ]
 
 
