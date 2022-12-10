336d1d9c62c74b41655bae192c7a92626131eafc
Debra Mazaya <debra.mazaya@ui.ac.id>
2022-12-10 13:14:38 +0700

    Implemented frontend for destination area page

--- jalankuy/settings.py
+++ jalankuy/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # This is the directory for storing `collectstatic` results.
 # This shouldn't be included in your Git repository.
